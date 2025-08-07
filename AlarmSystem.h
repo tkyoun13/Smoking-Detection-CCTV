423ff73405e7d0adc453a252e628df85baa1ba2d
Taegeun Yun <114209591+tkyoun13@users.noreply.github.com>
2025-08-07 20:13:58 +0900

    feat: AlarmSystem.h

---
+++ AlarmSystem.h
@@ -0,0 +1,17 @@
+﻿// 하드웨어 제어를 위한 클래스 (LED, Buzzer, Servo)
+#pragma once
+#include <wiringPi.h>
+#include <softTone.h>
+#include <softPwm.h>
+#include <vector>
+#include <iostream>
+
+class AlarmSystem {
+public:
+    AlarmSystem(int led, int buzzer, int servo);
+    void activate(); // 경고 시스템 동작
+
+private:
+    int ledPin, buzzerPin, servoPin;
+};
+
