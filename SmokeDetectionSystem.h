0d6061edc2ba0ab38bcec6935e8e484674dd9766
Taegeun Yun <114209591+tkyoun13@users.noreply.github.com>
2025-08-07 20:13:03 +0900

    feat: SmokeDetectionSystem.h

---
+++ SmokeDetectionSystem.h
@@ -0,0 +1,17 @@
+﻿// 시스템 전체를 통제하는 클래스
+#pragma once
+#include "VisionModel.h"
+#include "AlarmSystem.h"
+#include <opencv2/opencv.hpp>
+
+class SmokeDetectionSystem {
+public:
+    SmokeDetectionSystem(const std::string& model_path);
+    void run(); // 실시간 감지 루프 실행
+
+private:
+    VisionModel model;
+    AlarmSystem alarm;
+    cv::VideoCapture video;
+};
+
